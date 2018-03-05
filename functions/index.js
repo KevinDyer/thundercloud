b32558fbfc76fb5eb32065eee8187b9230f3fbb9
Kevin Dyer <kevin.m.dyer@gmail.com>
2018-03-04 18:52:51 -0700

    feat: use fetch instead of http library

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,13 @@
 (() => {
   'use strict';
 
+  const fetch = require('node-fetch');
   const functions = require('firebase-functions');
-  const admin = require('firebase-admin');
-  const http = require('http');
-  const url = require('url');
-
-  admin.initializeApp(functions.config().firebase);
 
   exports.updateSensor = functions.database.ref('/sessions/{sensorId}/{sessionId}/environment/data/{timestamp}').onWrite((event) => {
     const {sensorId, sessionId} = event.params;
     const sensorData = event.data.val();
-    console.log(`Device/Session[${sensorId}, ${sessionId}]: sensor data`, sensorData);
+    console.log(`Sensor/Session[${sensorId}, ${sessionId}]: sensor data`, sensorData);
     const {temperature} = sensorData;
     const body = {
       temperature: {
@@ -24,41 +20,21 @@
     return sendSensorUpdate(body);
   });
 
-  function sendSensorUpdate(body) {
-    return new Promise((resolve, reject) => {
-      const bodyBuffer = Buffer.from(JSON.stringify(body));
-      const {hostname, path} = url.parse(functions.config().backend.url);
-      const options = Object.assign({}, {
-        method: 'POST',
-        hostname: hostname,
-        path: path,
-        headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': bodyBuffer.length,
-        }
+  function sendSensorUpdate(data) {
+    const body = Buffer.from(JSON.stringify(data));
+    const input = functions.config().backend.url;
+    const init = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': body.length.toString(),
+      },
+      body: body,
+    };
+    return fetch(input, init)
+      .then((res) => {
+        console.log(res);
       });
-      const req = http.request(options, (res) => {
-        res.once('error', reject);
-
-        const chunks = [];
-        res.on('data', (chunk) => chunks.push(chunk));
-
-        res.on('end', () => {
-          const body = Buffer.concat(chunks);
-          const {status, message} = JSON.parse(body.toString());
-          if (200 === res.statusCode && 1 === status) {
-            console.log(`Success: ${message}`);
-            resolve();
-          } else {
-            console.log(`Failed: ${message}`);
-            reject(new Error(message));
-          }
-        });
-      });
-      req.once('error', reject);
-      req.write(bodyBuffer);
-      req.end();
-    });
   }
 
 })();
